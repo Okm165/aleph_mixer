776437351f5362a8e64a40bc9ad52f71f8462100
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2023-05-28 00:41:24 +0200

    feat: connect to azero ws

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+
+import { ApiPromise, WsProvider } from "@polkadot/api";
 
 const App: Component = () => {
-  return (
-    <div class="">
-      
-    </div>
-  );
+  async function main() {
+    // Initialise the provider to connect to the local node
+    const provider = new WsProvider("ws://rpc.test.azero.dev:9944");
+
+    // Create the API and wait until ready
+    const api = await ApiPromise.create({ provider });
+
+    // Retrieve the chain & node information information via rpc calls
+    const [chain, nodeName, nodeVersion] = await Promise.all([
+      api.rpc.system.chain(),
+      api.rpc.system.name(),
+      api.rpc.system.version(),
+    ]);
+
+    console.log(
+      `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
+    );
+  }
+
+  main()
+    .catch(console.error)
+    .finally(() => process.exit());
+
+  return <div class=""></div>;
 };
 
 export default App;
