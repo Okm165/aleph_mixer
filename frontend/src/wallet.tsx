4b673d4cf39ff98b3d26bc6e6fcfab5852b3b19c
Paweł <pavlnowak@gmail.com>
2023-05-28 12:50:46 +0200

    ContractPromise

--- frontend/src/wallet.tsx
+++ frontend/src/wallet.tsx
@@ -1,6 +1,9 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
-import { CodePromise } from '@polkadot/api-contract';
+import { ContractPromise } from '@polkadot/api-contract';
 
 // Connect to a Polkadot node
 const provider = new WsProvider('wss://rpc.polkadot.io');
 const api = await ApiPromise.create({ provider });
+
+const metadata = undefined;
+const contract = new ContractPromise(api, metadata, "");
